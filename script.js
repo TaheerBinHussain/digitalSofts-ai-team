2d63ec5d19e03cebf2485278c5ff141da11b52ba
Taheer Bin Hussain <taheerbinhussain@gmail.com>
2026-08-12 15:46:18 +0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Add event listener to nav links
-const navLinks = document.querySelectorAll('nav a');
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        console.log('Nav link clicked');
-    });
-});
